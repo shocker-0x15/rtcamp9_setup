3883ced9ccdf83a5aed056ed2d817a9a464c8708
shocker-0x15 <shocker.0x15@gmail.com>
2023-06-23 04:39:43 +0900

    added a script to enable inter-instance communication.

---
+++ enable_sshd.ps1
@@ -0,0 +1,39 @@
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# 一旦起動して初期設定ファイルを生成させる。
+Start-Service sshd
+
+# 自動起動設定有効化。
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# 明示的に公開鍵認証の有効化とパスワード認証の無効化を行う。
+$sshdConfigPath = Join-Path $env:ProgramData 'ssh\sshd_config'
+Get-Content $sshdConfigPath -Encoding Ascii -Raw | 
+    ForEach-Object {
+        $c = $_ -replace '#PubkeyAuthentication', 'PubkeyAuthentication'
+        $c = $c -replace '#PasswordAuthentication yes', 'PasswordAuthentication no'
+        $c | Out-File -FilePath $sshdConfigPath -Encoding ascii
+    }
+
+# サービス再起動。
+Restart-Service sshd
+
+# インスタンスメタデータから公開鍵を取得し administrators_authorized_keys に設定。
+$administratorsKeyPath = Join-Path $env:ProgramData 'ssh\administrators_authorized_keys'
+Invoke-RestMethod -Uri http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key/ |
+    Out-File -FilePath $administratorsKeyPath -Encoding ascii
+
+# ファイルのアクセスアクセス権の継承を解除。
+$acl = Get-Acl $administratorsKeyPath
+$acl.SetAccessRuleProtection($true, $true)
+$acl | Set-Acl -Path $administratorsKeyPath
+
+# ファイルの余計なアクセス権を削除。
+$acl = Get-Acl $administratorsKeyPath
+$ruleToRemove = $acl.Access |
+    Where-Object { $_.IdentityReference -eq 'NT AUTHORITY\Authenticated Users' }
+$acl.RemoveAccessRule($ruleToRemove)
+$acl | Set-Acl -Path $administratorsKeyPath
+
+# デフォルトシェルをPowershellに変更。
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
---
+++ set_up_inter_instance_ssh.ps1
@@ -0,0 +1,64 @@
+param (
+    # ローカルPCの秘密鍵のパス
+    [Parameter(Mandatory=$true)]
+    [string]$privateKey,
+
+    # インスタンス1のアドレス
+    [Parameter(Mandatory=$true)]
+    [string]$instPublicAddress1,
+    [Parameter(Mandatory=$true)]
+    [string]$instPrivateAddress1,
+
+    # インスタンス2のアドレス
+    [Parameter(Mandatory=$true)]
+    [string]$instPublicAddress2,
+    [Parameter(Mandatory=$true)]
+    [string]$instPrivateAddress2
+)
+
+$ec2UserName = "administrator"
+
+function genAndAppendPublicKey {
+    param(
+        [string]$clientInstPublicAddress,
+        [string]$serverInstPublicAddress,
+        [string]$serverInstPrivateAddress
+    )
+
+    Write-Output "----------------------------------------------------------------"
+
+    # クライアント側インスタンスでキーペアを作成、ローカルPCに公開鍵をダウンロードする。
+    $sshCommand = "ssh-keygen -f `$home/.ssh/id_rsa -t rsa -N '`"`"'"
+    # Write-Output ${sshCommand}
+    ssh -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress} ${sshCommand}
+    scp -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress}:.ssh/id_rsa.pub ./id_rsa.pub
+
+    # 公開鍵の内容をサーバー側インスタンスの認証済鍵リストに追加。
+    $publicKey = Get-Content ./id_rsa.pub -encoding ascii -raw
+    Remove-Item ./id_rsa.pub
+    $sshCommand = "Add-Content `${env:ProgramData}/ssh/administrators_authorized_keys '${publicKey}'"
+    # Write-Output $sshCommand
+    ssh -i ${privateKey} ${ec2UserName}@${serverInstPublicAddress} ${sshCommand}
+
+    # SSHサーバーをリスタート。
+    ssh -i ${privateKey} ${ec2UserName}@${serverInstPublicAddress} 'Restart-Service sshd'
+
+    # クライアント側インスタンスのknown_hostsにサーバーを登録。
+    $serverPublicKeyPath = "./ssh_host_ecdsa_key.pub"
+    scp -i ${privateKey} ${ec2UserName}@${serverInstPublicAddress}:C:\ProgramData\ssh\ssh_host_ecdsa_key.pub ${serverPublicKeyPath}
+    $serverPublicKey = Get-Content -Path ${serverPublicKeyPath} -Raw
+    Remove-Item ${serverPublicKeyPath}
+    $array = ${serverPublicKey} -split '\s+'
+    $knownHost = "${serverInstPrivateAddress} $($array[0]) $($array[1])"
+    $sshCommand = "Add-Content `$home/.ssh/known_hosts '${knownHost}'"
+    ssh -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress} ${sshCommand}
+
+    # インスタンス間SSH疎通確認。
+    $message = "SSH ${clientInstPublicAddress} -> ${serverInstPublicAddress} (private: ${serverInstPrivateAddress})"
+    Write-Output $message
+    $sshCommand = "ssh -i `$home\.ssh\id_rsa ${ec2UserName}@${serverInstPrivateAddress} 'echo Success!!'"
+    ssh -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress} ${sshCommand}
+}
+
+genAndAppendPublicKey ${instPublicAddress1} ${instPublicAddress2} ${instPrivateAddress2}
+genAndAppendPublicKey ${instPublicAddress2} ${instPublicAddress1} ${instPrivateAddress1}
