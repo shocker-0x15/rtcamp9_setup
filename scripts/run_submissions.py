29e192b995357c4787bf32103924c71efba8389f
shocker-0x15 <shocker.0x15@gmail.com>
2023-08-27 02:10:18 +0900

    updated the script to run submissions.

--- scripts/run_submissions.py
+++ scripts/run_submissions.py
@@ -17,6 +17,19 @@ def run_command(cmd, timeout=None):
     print(' '.join(cmd))
     return subprocess.run(cmd, check=True, timeout=timeout, capture_output=True, text=True)
 
+def run_remote_command(ssh, cmd, pr=True):
+    _, r_stdout, r_stderr = ssh.exec_command(cmd)
+    r_status = r_stdout.channel.recv_exit_status()
+    r_stdout = r_stdout.read().decode()
+    r_stderr = r_stderr.read().decode()
+    if pr:
+        print(f"Command exited with status: {r_status}")
+        if r_stdout:
+            print("Output:", r_stdout)
+        if r_stderr:
+            print("Errors:", r_stderr)
+    return r_status, r_stdout, r_stderr
+
 def run():
     key_path = R'C:\Users\Administrator\.ssh\id_rsa'
 
@@ -24,6 +37,7 @@ def run():
     result_dir = Path(sys.argv[2])
     primary_priv_ip = sys.argv[3]
     secondary_priv_ip = sys.argv[4]
+    copy_zip = bool(int(sys.argv[5]))
     dir_to_place = Path.home()
 
     if not submission_dir.exists():
@@ -42,30 +56,21 @@ def run():
     ssh.connect(secondary_priv_ip, username='administrator', pkey=key)
 
     # セカンダリーインスタンスのホームパスを取得。
-    r_cmd = f'echo $env:home'
-    _, r_stdout, r_stderr = ssh.exec_command(r_cmd)
-    exit_status = r_stdout.channel.recv_exit_status()
-    r_home_dir = r_stdout.read().decode().rstrip()
-    print(f"Command exited with status: {exit_status}")
-    print("Output:", r_home_dir)
-    print("Errors:", r_stderr.read().decode())
+    _, r_stdout, _ = run_remote_command(ssh, f'echo $env:home')
+    r_home_dir = r_stdout.rstrip()
 
-    # セカンダリーインスタンスのzip置き場を作成。
-    r_cmd = 'mkdir $home\\Desktop\\submissions'
-    _, r_stdout, r_stderr = ssh.exec_command(r_cmd)
-    exit_status = r_stdout.channel.recv_exit_status()
-    print(f"Command exited with status: {exit_status}")
-    print("Output:", r_stdout.read().decode())
-    print("Errors:", r_stderr.read().decode())
+    if copy_zip:
+        # セカンダリーインスタンスのzip置き場を作成。
+        run_remote_command(ssh, 'mkdir $home\\Desktop\\submissions')
 
-    # セカンダリーインスタンスにzipコピー。
-    sftp = ssh.open_sftp()
-    for entry in submission_dir.iterdir():
-        if not entry.is_file():
-            continue
-        sftp.put(entry,
-                 '{}\\Desktop\\submissions\\{}'.format(r_home_dir, entry.name))
-    sftp.close()
+        # セカンダリーインスタンスにzipコピー。
+        sftp = ssh.open_sftp()
+        for entry in submission_dir.iterdir():
+            if not entry.is_file():
+                continue
+            sftp.put(entry,
+                    '{}\\Desktop\\submissions\\{}'.format(r_home_dir, entry.name))
+        sftp.close()
 
     ssh.close()
 
@@ -84,39 +89,24 @@ def run():
         # zipをホームディレクトリに展開。
         with zipfile.ZipFile(entry, 'r') as zip_ref:
             ext_files = zip_ref.namelist()
-
-            num_dirs = 0
-            num_files = 0
-            for ext_file in ext_files:
-                if len(Path(ext_file).parts) != 1:
-                    continue
-                if Path(ext_file).is_file():
-                    num_files += 1
-                else:
-                    num_dirs += 1
-
-            if num_dirs == 1 and num_files == 0:
-                root_dir_name = ext_files[0]
-                zip_ref.extractall(dir_to_place)
-            else:
-                errors[str(entry)] = 'Failed in zip extraction.'
-                continue
+            root_dir_name = Path(ext_files[0]).parts[0]
+            zip_ref.extractall(dir_to_place)
 
         working_dir = dir_to_place / root_dir_name
         old_dir = chdir(working_dir)
 
-        dst_dir = result_dir / working_dir.name
+        dst_dir = result_dir / entry.stem
         dst_dir.mkdir(exist_ok=True)
-        img_dir = result_dir / ('images_' + working_dir.name)
+        dst_local_others_dir = dst_dir / 'local_others'
+        dst_local_others_dir.mkdir(exist_ok=True)
+        dst_remote_others_dir = dst_dir / 'remote_others'
+        dst_remote_others_dir.mkdir(exist_ok=True)
+        img_dir = result_dir / ('images_' + entry.stem)
         img_dir.mkdir(exist_ok=True)
 
-        # リモートPCでzipをホームディレクトリに展開。
+        # リモートマシンでzipをホームディレクトリに展開。
         r_cmd = 'Expand-Archive -Path {}\\Desktop\\submissions\\{} -DestinationPath {}'.format(r_home_dir, entry.name, r_home_dir)
-        _, r_stdout, r_stderr = ssh.exec_command(r_cmd)
-        exit_status = r_stdout.channel.recv_exit_status()
-        print(f"Command exited with status: {exit_status}")
-        print("Output:", r_stdout.read().decode())
-        print("Errors:", r_stderr.read().decode())
+        run_remote_command(ssh, r_cmd)
 
         # スライドのコピー。
         deck_files = []
@@ -124,6 +114,27 @@ def run():
         deck_files += working_dir.glob('*.pptx')
         for deck_file in deck_files:
             shutil.copy2(deck_file, dst_dir)
+
+        def get_local_file_list(dir):
+            return set([file.name for file in dir.iterdir() if file.is_file()])
+
+        def get_remote_file_list(dir):
+            r_cmd = f'Get-ChildItem -Path {dir} -File | Select-Object -ExpandProperty Name'
+            _, r_stdout, _ = run_remote_command(ssh, r_cmd)
+            return set(r_stdout.splitlines())
+
+        # ローカルマシンのレンダリング実行前のファイルリストを取得。
+        org_files = get_local_file_list(working_dir)
+
+        # リモートマシンのレンダリング実行前のファイルリストを取得。
+        r_org_files = get_remote_file_list(f'$home\\{root_dir_name}')
+
+        # requirements.txtに従ってパッケージインストール。
+        if 'requirements.txt' in org_files:
+            cmd = ['pip', 'install', '-r', 'requirements.txt']
+            run_command(cmd)
+        if 'requirements.txt' in r_org_files:
+            run_remote_command(ssh, f'pip install -r $home\\{root_dir_name}\\requirements.txt')
 
         # レンダリング実行。
         exe_success = True
@@ -152,26 +163,40 @@ def run():
             with open(dst_dir / 'stderr.log', 'w') as f:
                 f.write(stderr)
 
+        misc_msg = ''
         if exe_success:
             # fpsを読み取る。
-            with open('fps.txt', 'r') as f:
-                fps = int(f.read())
+            if (working_dir / 'fps.txt').exists():
+                with open('fps.txt', 'r') as f:
+                    fps = int(f.read())
+            else:
+                misc_msg += 'fps.txt: not found\n'
+                fps = 60
+
+            # ローカルマシンのレンダリング実行後のファイルリストを取得。
+            files = get_local_file_list(working_dir)
+            new_files = files - org_files
+
+            # リモートマシンのレンダリング実行後のファイルリストを取得。
+            r_files = get_remote_file_list(f'$home\\{root_dir_name}')
+            r_new_files = r_files - r_org_files
 
             # ローカルマシンで出力された画像をコピー。
-            for file in working_dir.iterdir():
-                if img_regex.match(file.name):
-                    shutil.copy2(file, img_dir)
-
-            r_cmd = f'Get-ChildItem -Path $home\\{root_dir_name} | Select-Object -ExpandProperty Name'
-            _, r_stdout, _ = ssh.exec_command(r_cmd)
-            r_files = r_stdout.read().decode().splitlines()
+            for file in new_files:
+                if img_regex.match(file):
+                    shutil.copy2(working_dir / file, img_dir)
+                else:
+                    shutil.copy2(working_dir / file, dst_local_others_dir)
 
             # リモートマシンで出力された画像をコピー。
             sftp = ssh.open_sftp()
-            for file in r_files:
+            for file in r_new_files:
                 if img_regex.match(file):
                     sftp.get('{}\\{}\\{}'.format(r_home_dir, root_dir_name, file),
                              '{}\\{}'.format(img_dir, file))
+                else:
+                    sftp.get('{}\\{}\\{}'.format(r_home_dir, root_dir_name, file),
+                             '{}\\{}'.format(dst_remote_others_dir, file))
             sftp.close()
 
             # 連番画像における欠落事故防止のため名前でソートしたのち改めて連番化する。
@@ -185,23 +210,26 @@ def run():
             for idx, file in enumerate(img_list):
                 Path(file).rename(Path(file).with_name(f'input_{idx:03}{ext}'))
 
-            # 動画作成。
-            cmd = ['ffmpeg']
-            # Opitions for input
-            cmd += ['-framerate', str(fps), '-i', f'{img_dir}\\input_%03d{ext}']
-            # Options for output
-            cmd += ['-c:v', 'libx264', '-crf', '18', f'{dst_dir}\\result.mp4']
-            run_command(cmd)
+            if len(img_list) > 0:
+                # 動画作成。
+                cmd = ['ffmpeg']
+                # Opitions for input
+                cmd += ['-framerate', str(fps), '-i', f'{img_dir}\\input_%03d{ext}']
+                # Options for output
+                cmd += ['-c:v', 'libx264', '-crf', '18', f'{dst_dir}\\result.mp4']
+                run_command(cmd)
+
+        if len(img_list) == 0:
+            misc_msg += 'No images.\n'
+        
+        with open(dst_dir / 'misc.txt', 'w') as f:
+            f.write(misc_msg)
 
         chdir(old_dir)
 
-        # リモートPCで展開したディレクトリを削除。
+        # リモートマシンで展開したディレクトリを削除。
         r_cmd = f'Remove-Item -Recurse $home\\{root_dir_name}'
-        _, r_stdout, r_stderr = ssh.exec_command(r_cmd)
-        exit_status = r_stdout.channel.recv_exit_status()
-        print(f"Command exited with status: {exit_status}")
-        print("Output:", r_stdout.read().decode())
-        print("Errors:", r_stderr.read().decode())
+        run_remote_command(ssh, r_cmd)
 
         ssh.close()
 
--- scripts/set_up_inter_instance_ssh.ps1
+++ scripts/set_up_inter_instance_ssh.ps1
@@ -13,7 +13,10 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$instPublicAddress1,
     [Parameter(Mandatory=$true)]
-    [string]$instPrivateAddress1
+    [string]$instPrivateAddress1,
+
+    # インスタンス上でキーペアの生成を行う
+    [bool]$genKey=$true
 )
 
 $ec2UserName = "administrator"
@@ -22,26 +25,29 @@ function genAndAppendPublicKey {
     param(
         [string]$clientInstPublicAddress,
         [string]$serverInstPublicAddress,
-        [string]$serverInstPrivateAddress
+        [string]$serverInstPrivateAddress,
+        [bool]$genKey = $true
     )
 
     Write-Output "----------------------------------------------------------------"
 
-    # クライアント側インスタンスでキーペアを作成、ローカルPCに公開鍵をダウンロードする。
-    $sshCommand = "ssh-keygen -f `$home/.ssh/id_rsa -t rsa -N '`"`"'"
-    # Write-Output ${sshCommand}
-    ssh -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress} ${sshCommand}
-    scp -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress}:.ssh/id_rsa.pub ./id_rsa.pub
+    if ($genKey) {
+        # クライアント側インスタンスでキーペアを作成、ローカルPCに公開鍵をダウンロードする。
+        $sshCommand = "ssh-keygen -f `$home/.ssh/id_rsa -t rsa -N '`"`"'"
+        # Write-Output ${sshCommand}
+        ssh -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress} ${sshCommand}
+        scp -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress}:.ssh/id_rsa.pub ./id_rsa.pub
 
-    # 公開鍵の内容をサーバー側インスタンスの認証済鍵リストに追加。
-    $publicKey = Get-Content ./id_rsa.pub -encoding ascii -raw
-    Remove-Item ./id_rsa.pub
-    $sshCommand = "Add-Content `${env:ProgramData}/ssh/administrators_authorized_keys '${publicKey}'"
-    # Write-Output $sshCommand
-    ssh -i ${privateKey} ${ec2UserName}@${serverInstPublicAddress} ${sshCommand}
+        # 公開鍵の内容をサーバー側インスタンスの認証済鍵リストに追加。
+        $publicKey = Get-Content ./id_rsa.pub -encoding ascii -raw
+        Remove-Item ./id_rsa.pub
+        $sshCommand = "Add-Content `${env:ProgramData}/ssh/administrators_authorized_keys '${publicKey}'"
+        # Write-Output $sshCommand
+        ssh -i ${privateKey} ${ec2UserName}@${serverInstPublicAddress} ${sshCommand}
 
-    # SSHサーバーをリスタート。
-    ssh -i ${privateKey} ${ec2UserName}@${serverInstPublicAddress} 'Restart-Service sshd'
+        # SSHサーバーをリスタート。
+        ssh -i ${privateKey} ${ec2UserName}@${serverInstPublicAddress} 'Restart-Service sshd'
+    }
 
     # クライアント側インスタンスのknown_hostsにサーバーを登録。
     $serverPublicKeyPath = "./ssh_host_ecdsa_key.pub"
@@ -60,5 +66,5 @@ function genAndAppendPublicKey {
     ssh -i ${privateKey} ${ec2UserName}@${clientInstPublicAddress} ${sshCommand}
 }
 
-genAndAppendPublicKey ${instPublicAddress0} ${instPublicAddress1} ${instPrivateAddress1}
-genAndAppendPublicKey ${instPublicAddress1} ${instPublicAddress0} ${instPrivateAddress0}
+genAndAppendPublicKey ${instPublicAddress0} ${instPublicAddress1} ${instPrivateAddress1} ${genKey}
+genAndAppendPublicKey ${instPublicAddress1} ${instPublicAddress0} ${instPrivateAddress0} ${genKey}
